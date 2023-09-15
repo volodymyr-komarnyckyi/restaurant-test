896262dadac09d965dfc19a3a97456dc933a3637
user <volodimirkomarnickij28@gmail.com>
2023-09-15 18:47:52 +0300

    Implemented basic views and serializers for Restaurant and Menu models. Added endpoints for them

---
+++ restaurant/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from restaurant.models import (
+    Restaurant,
+    Menu,
+)
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = "__all__"
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = "__all__"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from restaurant.models import (
+    Restaurant,
+    Menu
+)
+from restaurant.serializers import (
+    RestaurantSerializer,
+    MenuSerializer,
+)
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
