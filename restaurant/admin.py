536599b4b9472e2cf94edccb866b06003b652546
user <volodimirkomarnickij28@gmail.com>
2023-09-15 17:52:12 +0300

    Created basic models for Restaurant and Menu and registered them in admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from restaurant.models import (
+    Restaurant,
+    Menu,
+)
+
+admin.site.register(Restaurant)
+admin.site.register(Menu)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="menus"
+    )
+    date = models.DateField()
+    items = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"Menu for {self.restaurant.name} with items: {self.items}"
+
+
+# class Vote(models.Model):
+#     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+#     vote = models.IntegerField(choices=[(1, 'Yes'), (2, 'No')])
