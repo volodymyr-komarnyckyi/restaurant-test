4924120a2ef2ebd62e61a04edc498fa9312d61d3
user <volodimirkomarnickij28@gmail.com>
2023-09-16 15:51:50 +0300

    Removed unnecessary comments

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -21,10 +21,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "drf_spectacular",
     "pytest",
     "pytest_django",
+
     "restaurant",
     "user",
 ]
